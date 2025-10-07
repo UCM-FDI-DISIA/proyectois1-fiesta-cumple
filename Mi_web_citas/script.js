35809f954be460389df57053f42ccf83c6b8b870
DavidTijerin <dtijerin@ucm.es>
2025-10-07 23:03:50 +0200

    Inicio de la creación de la interfaz para iniciar sesión.

--- Mi_web_citas/script.js
+++ Mi_web_citas/script.js
@@ -1,3 +1,10 @@
 document.getElementById("botonSaludo").addEventListener("click", function() {
   alert("¡Hola! Gracias por visitar mi página 😄");
 });
+
+// Muestra la ventana de iniciar sesión al pulsar el botón 
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('botonIniciarSesion').onclick = function() {
+    document.getElementById('ventanaIS').style.display = 'flex';
+  };
+});
